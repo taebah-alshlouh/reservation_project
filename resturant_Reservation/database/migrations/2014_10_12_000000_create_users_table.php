d621aa86d22d0ba26c05744f427a25bb72be0149
unknown <taebah.bassam@gmail.com>
2023-01-28 01:55:38 +0300

    Admin Seeder

--- resturant_Reservation/database/migrations/2014_10_12_000000_create_users_table.php
+++ resturant_Reservation/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
